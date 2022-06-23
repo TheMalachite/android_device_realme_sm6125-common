58ca5e9a7323d3a249689872aeef49c6e8a22055
TheMalachite <eliasgheeraert@gmail.com>
2022-06-23 07:56:33 +0200

    r5x: LineageOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_r5x.mk
+    $(LOCAL_DIR)/lineage_r5x.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_r5x-user \
-    arrow_r5x-userdebug \
-    arrow_r5x-eng
+    lineage_r5x-user \
+    lineage_r5x-userdebug \
+    lineage_r5x-eng
--- lineage_r5x.mk
+++ lineage_r5x.mk
@@ -9,21 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Arrow stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
-ARROW_GAPPS := true
-TARGET_BOOT_ANIMATION_RES := 720
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-PRODUCT_BRAND := Realme
+PRODUCT_BRAND := realme
 PRODUCT_DEVICE := r5x
-PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := arrow_r5x
+PRODUCT_MANUFACTURER := realme
+PRODUCT_NAME := lineage_r5x
 PRODUCT_MODEL := Realme 5 Series
 
-PRODUCT_GMS_CLIENTID_BASE := android-realme
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 TARGET_VENDOR_PRODUCT_NAME := r5x
 TARGET_VENDOR_DEVICE_NAME := r5x
